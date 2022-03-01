a7f5467ce8bde2df66615e3d7cb6f877aad2cb40
Iqbal Hossan <engiqbal1999@gmail.com>
2022-03-01 09:15:42 +0600

    phones have displayed

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,7 +12,25 @@ const searchPhones = () => {
 // display phones
 const displayPhones = (phones) => {
 
-    phones.forEach(phone => {
-        console.log(phone)
+    phones.map(phone => {
+        // console.log(phone)
+        const searchResultDiv = document.getElementById('search-result');
+        const createDisplayDiv = document.createElement('div');
+        createDisplayDiv.classList.add('col');
+        // createDisplayDiv.classList.add(' bg-danger');
+        createDisplayDiv.innerHTML = `                       
+            <div class="card ">
+                <img  src="${phone.image}" class="card-img-top figure-img img-fluid rounded p-3" alt="...">
+                <div class="card-body">
+                    <h6 class="mt-3">${phone.phone_name}</h6>
+                    <h5>${phone.brand}</h5> 
+                </div>
+                <div class="card-footer">
+                    <button>See Details</button>
+                </div>
+            </div>
+        `;
+
+        searchResultDiv.appendChild(createDisplayDiv)
     });
 }
