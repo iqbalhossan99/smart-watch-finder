1b0805eaa369bc46637a1d29bc52a51beaa2b390
Iqbal Hossan <engiqbal1999@gmail.com>
2022-03-01 08:46:53 +0600

    api link is wirking properlly now

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-console.log('this is okay')
+// search phones
+const searchPhones = () => {
+    const inputField = document.getElementById('input-field');
+    const inputValue = inputField.value;
+
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
+        .then(res => res.json())
+        .then(data => displayPhones(data.data))
+}
+
+
+// display phones
+const displayPhones = (phones) => {
+
+    phones.forEach(phone => {
+        console.log(phone)
+    });
+}
